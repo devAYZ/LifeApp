d6c97b1e6de7d2b6d230b833c1cbc2ff4f7affc1
devAYZ <fatokimiemman@gmail.com>
2024-09-18 04:18:23 +0100

    Initial Commit

---
+++ LifeApp/LifeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeAppApp.swift
+//  LifeApp
+//
+//  Created by Ayokunle Pro on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct LifeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
